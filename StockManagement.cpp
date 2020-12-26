de86b1951c0d3aed2a9f97e55fd5d61b64355edf
hujkc123 <200822041@qq.com>
2020-12-26 23:17:36 +0800

    =change book to product

--- StockManagement.cpp
+++ StockManagement.cpp
@@ -5,7 +5,7 @@
 #include <iterator> 
 #include <vector> 
 using namespace std;
-class Book{
+class Product{
     private:
     char name[15];
     char producer[15];
@@ -17,7 +17,7 @@ class Book{
     void printInformation(){
         cout<<id<<name<<producer<<date<<price<<amount;
     }
-    Book(int id,char* name,char* producer,char* date,double price,int amount){
+    Product(int id,char* name,char* producer,char* date,double price,int amount){
         this->id=id;
         strcpy(this->name,name);
         strcpy(this->producer,producer);
@@ -65,102 +65,102 @@ class Book{
         return this->amount;
     }
 };
-static vector<Book> market;
-bool cunzai(int id){
-   bool zhaodao=false;
-    for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+static vector<Product> market;
+bool ifExists(int id){
+   bool foundOut=false;
+    for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
         if(id==(i->getId())){
-            zhaodao=true;
+            foundOut=true;
         }
     }
-    return zhaodao;
+    return foundOut;
 }
-void showBook(){
-    cout<<"##################书本列表########################"<<endl;
-        for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
-        cout<<"书本id:"<<i->getId()<<"\t"<<"书本名称:"<<i->getName()<<"\t"<<"书本出版商"<<i->getProducer()<<"\t"<<"书本出版日期:"<<i->getDate()<<"\t"<<"书本价格:"<<i->getPrice()<<"\t"<<"书本数量:"<<i->getAmount()<<endl;
+void showProduct(){
+    cout<<"##################商品列表########################"<<endl;
+        for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
+        cout<<"商品id:"<<i->getId()<<"\t"<<"商品名称:"<<i->getName()<<"\t"<<"商品生产商"<<i->getProducer()<<"\t"<<"商品生产日期:"<<i->getDate()<<"\t"<<"商品价格:"<<i->getPrice()<<"\t"<<"商品数量:"<<i->getAmount()<<endl;
     }
 }
 void showMenus(){
     cout<<"########系统菜单###########\n"<<endl;
-    cout<<"      1.书本入库           "<<endl;
-    cout<<"      2.书本出库           "<<endl;
-    cout<<"      3.删除书本           "<<endl;
-    cout<<"      4.修改书本           "<<endl;
-    cout<<"      5.查询书本           "<<endl;
-    cout<<"      6.浏览书本           "<<endl;
-    cout<<"      7.书本统计           "<<endl;
-    cout<<"      8.书本排序           "<<endl;
+    cout<<"      1.商品入库           "<<endl;
+    cout<<"      2.商品出库           "<<endl;
+    cout<<"      3.删除商品           "<<endl;
+    cout<<"      4.修改商品           "<<endl;
+    cout<<"      5.查询商品           "<<endl;
+    cout<<"      6.浏览商品           "<<endl;
+    cout<<"      7.商品统计           "<<endl;
+    cout<<"      8.商品排序           "<<endl;
     cout<<"      9.退出程序           "<<endl;
 }
 
-void inputBook(){
+void inputProduct(){
     int id;
     char name[15];
     char producer[15];
     char date[15];
     double price;
     int amount;
-    cout<<"请输入书本id:"<<endl;
+    cout<<"请输入商品id:"<<endl;
     cin>>id;
-    cout<<"请输入书本名称:"<<endl;
+    cout<<"请输入商品名称:"<<endl;
     cin>>name;
-    cout<<"请输入书本出版商:"<<endl;
+    cout<<"请输入商品生产商:"<<endl;
     cin>>producer;
-    cout<<"请输入书本出版日期"<<endl;
+    cout<<"请输入商品生产日期"<<endl;
     cin>>date;
-    cout<<"请输入书本价格"<<endl;
+    cout<<"请输入商品价格"<<endl;
     cin>>price;
-    cout<<"请输入书本数量"<<endl;
+    cout<<"请输入商品数量"<<endl;
     cin>>amount;
-    Book Book(id,name,producer,date,price,amount);
-    market.push_back(Book);
+    Product Product(id,name,producer,date,price,amount);
+    market.push_back(Product);
     cout<<"添加成功！"<<endl;
 }
-void modifyBook(){
+void modifyProduct(){
     int id;
     char name[15];
     char producer[15];
     char date[15];
     double price;
     int amount;
-    cout<<"请输入要修改的书本id:"<<endl;
+    cout<<"请输入要修改的商品id:"<<endl;
     cin>>id;
-    if(!cunzai(id)){
-        cout<<"不存在此书本无法修改"<<endl;
+    if(!ifExists(id)){
+        cout<<"不存在此商品无法修改"<<endl;
         return;
     }
-    cout<<"请输入书本名称:"<<endl;
+    cout<<"请输入商品名称:"<<endl;
     cin>>name;
-    cout<<"请输入书本出版商:"<<endl;
+    cout<<"请输入商品生产商:"<<endl;
     cin>>producer;
-    cout<<"请输入书本出版日期"<<endl;
+    cout<<"请输入商品生产日期"<<endl;
     cin>>date;
-    cout<<"请输入书本价格"<<endl;
+    cout<<"请输入商品价格"<<endl;
     cin>>price;
-    cout<<"请输入书本数量"<<endl;
+    cout<<"请输入商品数量"<<endl;
     cin>>amount;
-    Book Book(id,name,producer,date,price,amount);
-    for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+    Product Product(id,name,producer,date,price,amount);
+    for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
         if(id==(i->getId())){
             market.erase(i);
         }
     }
-    market.push_back(Book);
+    market.push_back(Product);
 }
-void outputBook(){
+void outputProduct(){
     if(market.size()==0){
-        cout<<"无法出库，请先添加书本！"<<endl;
+        cout<<"无法出库，请先添加商品！"<<endl;
         return;
     }
-    showBook();
+    showProduct();
     int id;
-    cout<<"请输入需要出库的书本id"<<endl;
+    cout<<"请输入需要出库的商品id"<<endl;
     cin>>id;
-        bool zhaodao=false;
-        for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+        bool foundOut=false;
+        for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
         if(id==(i->getId())){
-            zhaodao=true;
+            foundOut=true;
             if(0==(i->getAmount())){
                 cout<<"库存不够"<<endl;
                 break;
@@ -170,70 +170,70 @@ void outputBook(){
             }
         }
     }
-    if(zhaodao){
-        cout<<"书本id为"<<id<<"的书本成功出库!"<<endl;
+    if(foundOut){
+        cout<<"商品id为"<<id<<"的商品成功出库!"<<endl;
     }
 }
-void deleteBook(){
+void deleteProduct(){
         if(market.size()==0){
-        cout<<"无法删除书本，请先添加书本！"<<endl;
+        cout<<"无法删除商品，请先添加商品！"<<endl;
         return;
     }
-    showBook();
+    showProduct();
     int id;
-    cout<<"请输入需要删除的书本id"<<endl;
+    cout<<"请输入需要删除的商品id"<<endl;
     cin>>id;
-    bool zhaodao=false;
-        for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+    bool foundOut=false;
+        for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
         if(id==i->getId()){
-            zhaodao=true;
+            foundOut=true;
             market.erase(i);
         }
     }
-    if(zhaodao){
-        cout<<"成功删除id为"<<id<<"的书本"<<endl;
+    if(foundOut){
+        cout<<"成功删除id为"<<id<<"的商品"<<endl;
     }else{
-        cout<<"无法删除id为"<<id<<"的书本，此id在书本库存中不存在！"<<endl; 
+        cout<<"无法删除id为"<<id<<"的商品，此id在商品库存中不存在！"<<endl; 
     }
 }
-void searchBook(){
+void searchProduct(){
     string str;
-    cout<<"请输入书本名称以搜索："<<endl;
+    cout<<"请输入商品名称以搜索："<<endl;
     cin>>str;
-    bool zhaodao=false;
-   for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+    bool foundOut=false;
+   for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
        string str1=i->getName();
         if(str1.find(str)!=string::npos){
-            zhaodao=true;
-             cout<<"书本id:"<<i->getId()<<"\t"<<"书本名称:"<<i->getName()<<"\t"<<"书本出版商"<<i->getProducer()<<"\t"<<"书本出版日期:"<<i->getDate()<<"\t"<<"书本价格:"<<i->getPrice()<<"\t"<<"书本数量:"<<i->getAmount()<<endl;
+            foundOut=true;
+             cout<<"商品id:"<<i->getId()<<"\t"<<"商品名称:"<<i->getName()<<"\t"<<"商品生产商"<<i->getProducer()<<"\t"<<"商品生产日期:"<<i->getDate()<<"\t"<<"商品价格:"<<i->getPrice()<<"\t"<<"商品数量:"<<i->getAmount()<<endl;
         }
    }
-   if(!zhaodao){
-    cout<<"没有找到相应的书本名称！"<<endl;
+   if(!foundOut){
+    cout<<"没有找到相应的商品名称！"<<endl;
    }
 }
-void totalBook(){
-    int totalBooks=0;
+void totalProduct(){
+    int totalProducts=0;
     int totalCategories=0;
     double totalMoney=0;
-        bool zhaodao=false;
-   for(vector<Book>::iterator i=market.begin();i!=market.end();i++){
+        bool foundOut=false;
+   for(vector<Product>::iterator i=market.begin();i!=market.end();i++){
      int amount=i->getAmount();
      double money=i->getPrice();
      totalCategories++;
      totalMoney+=amount*money;
-     totalBooks+=amount;
+     totalProducts+=amount;
    }
-   cout<<"一共"<<totalCategories<<"种不同的书本,一共"<<totalBooks<<"件书本(总库存),总价值是："<<totalMoney<<endl;
+   cout<<"一共"<<totalCategories<<"种不同的商品,一共"<<totalProducts<<"件商品(总库存),总价值是："<<totalMoney<<endl;
 }
-static bool myCompare(const Book& a1,const Book& a2)
+static bool myCompare(const Product& a1,const Product& a2)
 {
     return a1.id<=a2.id;
 }
-void sortBook(){
+void sortProduct(){
     cout<<"按照id大小排序："<<endl;
     sort(market.begin(), market.end(), myCompare);
-   showBook();
+   showProduct();
 }
 
 int main(){
@@ -244,35 +244,35 @@ int main(){
     bool flag=false;
     switch(num){
         case 1:{
-            inputBook();
+            inputProduct();
             break;
         }
         case 2:{
-         outputBook();
+         outputProduct();
          break;
         }
                 case 3:{
-         deleteBook();
+         deleteProduct();
          break;
         }
                 case 4:{
-         modifyBook();
+         modifyProduct();
          break;
         }
                 case 5:{
-         searchBook();
+         searchProduct();
          break;
         }
          case 6:{
-         showBook();
+         showProduct();
          break;
         }
          case 7:{
-         totalBook();
+         totalProduct();
          break;
         }
         case 8:{
-         sortBook();
+         sortProduct();
          break;
         }
         case 9:{
